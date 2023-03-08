2316fbf3531bcd9356f496960766a505590e2fad
jmkmac <45799254+jmkmac@users.noreply.github.com>
2023-03-08 14:30:06 +0000

    Added a loop to the identity section.

--- main.py
+++ main.py
@@ -1,55 +1,65 @@
-#if/else statements applied to Chatbot 2/22/2023
-#Virtual Chatbot to check in patients at a Hospital
+# Adding Sentinel Loops 3/7/2023
+# Virtual Chatbot to check in patients at a Hospital
 print('Hello Welcome to the Hospital Virtual Check In!')
 print('\n')
 
-#Question to collects patient information
+# Get name of Patients
 name = input('Who am I speaking with?:')
 print('Hello', name)
 print('\n')
 
-#How do you Identify sequence structure with the martian age and fare decision structure 
-sex1 = '1'
-sex2 = '2'
-sex3 = '3'
-sex = input('Do you identify as (1):Male, (2):Female or (3):Martian?:')
-if sex == sex1:
-  print(name,'You Identify as a Male.\n')
-  age = int(input('What is your age?:'))
-  print(name,'you are',age,'years old.\n')
-else:
-  if sex == sex2:
-    print(name,'you identify as a Female.\n')
+# How you identify loop
+print('Hello tell us how you identify?')
+sex = int(input('Do you identify as a (1):Male, (2):Female or (3):Martian?:'))
+print('\n')
+while sex > 3:
+  print('Hello tell us how you identify?')
+  sex = int(input('Do you identify as a (1):Male, (2):Female or (3):Martian?:'))
+  
+  if sex == 1:
+    print(name, 'You Identify as a Male.')
+    print('\n')
     age = int(input('What is your age?:'))
-    print(name,'you are',age,'years old.\n')
+    print(name, 'you are', age, 'years old.')
   else:
-    if sex == sex3:
-      print(name,'you identify as a Martian!\n')
-      age = int(input('What is your age?:'))
-      martian_age = age / 1.88 #Martian age calculation
-      if age <=12:
-        print(name,f'Did you know your age on the planet Mars would be {martian_age:.0f} years old!') 
-        print('If you want to travel back to Mars your fare would be $5,000!')
-      elif age >=19 and age <=65:
-        print(name,f'Did you know your age on the planet Mars would be {martian_age:.0f} years old!')
-        print('If you want to travel back to Mars your fare would be $10,000!')
-      else:
-        print(name,f'Did you know your age on the planet Mars would be {martian_age:.0f} years old!')
-        print('If you want to travel back to Mars your fare would be $7,000!')
-    if sex > sex3:
-      print('Please pick a number that corralates to how you identify!')
-    else:
+    if sex == 2:
+      print(name, 'you identify as a Female.')
       print('\n')
-      
+      age = int(input('What is your age?:'))
+      print(name, 'you are', age, 'years old.')
+    else:
+      # Martian Age and Fare to get back from Mars
+      if sex == 3:
+        print(name, 'you identify as a Martian!')
+        print('\n')    
+        age = int(input('What is your age?:'))
+        martian_age = age / 1.88  #Martian age calculation
+        if age <= 12:
+          print(name,f'Did you know your age on the planet Mars would be {martian_age:.0f} years old!')
+          print('If you want to travel back to Mars your fare would be $5,000!')
+          print('\n')
+        elif age >= 19 and age <= 65:
+          print(name,f'Did you know your age on the planet Mars would be {martian_age:.0f} years old!')
+          print('If you want to travel back to Mars your fare would be $10,000!')
+          print('\n')
+        else:
+          print(name,f'Did you know your age on the planet Mars would be {martian_age:.0f} years old!')
+          print('If you want to travel back to Mars your fare would be $7,000!')
+          print('\n')
+        
 #Address information
 house_number = int(input('What is your house number?:'))
 street = input('What is your street name?:')
 city = input('What City do you live?:')
 state = input('What State do you ive?:')
 zip = int(input('What is your zip code?:'))
-print(name, 'you live at:', house_number, street, city, state, zip)
+print(name, 'you live at:', house_number,
+street, city, state, zip)
 print('\n')
-print(
-  'Thank you', name,
-  'for entering your patient information.  Please have a sit and we will be with you shortly.'
-)
+
+  
+#Exit Chatbot
+print('Thank you', name,'for entering your patient information.  Please have a sit and we will be with you shortly.')
+
+# Quit Application Function
+quit('Thank you now exiting check-in')
